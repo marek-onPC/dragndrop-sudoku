1f1c59df8a170d0010fbeda920347b0f0e8dd488
marek-onPC <mareksmieja@gmail.com>
2021-05-08 11:52:49 +0200

    Init and configuration

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Home from './views/Home';
+import Game from './views/Game';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/game" component={Game} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/views/Game.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Game extends PureComponent {
+  render() {
+    return (
+      <div className="game">
+        <p>Game</p>
+      </div>
+    )
+  }
+}
